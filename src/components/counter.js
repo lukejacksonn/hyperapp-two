5b7f0f71d9f6fd1414e5b79a18d94db17b20009d
Luke Jackson <lukejacksonn@gmail.com>
2017-02-27 16:33:47 -0500

    Switches to new action syntax a.b() which negates hyperapp-unite

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,7 +1,7 @@
 import { h } from 'hyperapp'
 export default ({m,a}) =>
   <counter->
-    <button onclick={e => a.decCounter()}>DEC</button>
+    <button onclick={e => a.counter.dec()}>DEC</button>
     <count->{m.count}</count->
-    <button onclick={e => a.incCounter()}>INC</button>
+    <button onclick={e => a.counter.inc()}>INC</button>
   </counter->
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
-import { uniteActions } from 'hyperapp-unite'
-
 import counter from './_counter'
 import todos from './_todos'
 
-export default uniteActions({
+export default ({
   counter,
   todos,
 })
