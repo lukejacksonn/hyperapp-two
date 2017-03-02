e252892174ad3181def02dd82dfe74c119ae35e2
Luke Jackson <lukejacksonn@gmail.com>
2017-03-02 16:11:48 -0500

    Changes onkeyup to oninput for todos input

--- src/components/todos.js
+++ src/components/todos.js
@@ -2,8 +2,8 @@ import { h } from 'hyperapp'
 import Todo from './todo'
 export default ({m,a}) =>
   <todos->
-    <form onsubmit={e => a.r.todos.add({ text: m.input }) && e.preventDefault()}>
-      <input placeholder='Enter todo text' value={m.input} onkeyup={e => a.r.todos.input(e.target.value)} />
+    <form onsubmit={e => a.r.todos.add({text: m.input}) && e.preventDefault()}>
+      <input placeholder='Enter todo text' value={m.input} oninput={e => a.r.todos.input(e.target.value)} />
       <button disabled={m.input.length > 0 ? false : true}>+Todo</button>
     </form>
     <ul>{m.list.map(x => <Todo m={x} a={a} />)}</ul>
